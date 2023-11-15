06cdc4557163b6f9ba1252ae9ff529aa1ed27fb1
Sergei <40200071+Sergei1790@users.noreply.github.com>
2023-11-15 14:32:30 +0200

    new

--- scripts.js
+++ scripts.js
@@ -45,23 +45,60 @@ numberBtns.forEach(numberBtn => {
     });
 });
 function toSecondNum(){
-    operator = this.innerText;
-    firstNumber = Number(firstNumber);
-    if(currentScreen.innerText.at(-1) === '+' ||
-    currentScreen.innerText.at(-1) === '-' ||
-    currentScreen.innerText.at(-1) === '×' ||
-    currentScreen.innerText.at(-1) === '÷'
-    ){
-        currentScreen.innerText = currentScreen.innerText.slice(0, -1) + operator; 
-    } else{
-        currentScreen.innerText += operator;
-    }
-    if (result !== false) {
-        secondNumber = '';
-        result = false;
-    } else if (secondNumber !== '') {
+    if (secondNumber !== '' && result == false) {
         operate();
+
+       
+        // if (result !== false) {
+        //     secondNumber = '';
+        //     result = false;
+        // } 
     }
+    secondNumber = '';
+    result = false;
+    if (secondNumber == '') {
+        operator = this.innerText;
+        firstNumber = Number(firstNumber);
+        if(currentScreen.innerText.at(-1) === '+' ||
+        currentScreen.innerText.at(-1) === '-' ||
+        currentScreen.innerText.at(-1) === '×' ||
+        currentScreen.innerText.at(-1) === '÷'
+        ){
+            currentScreen.innerText = currentScreen.innerText.slice(0, -1) + operator; 
+        } else{
+            currentScreen.innerText += operator;
+        }
+    }
+    
+
+//     нажимаем числа, которые добавляються в num1 если оно строка, а 
+//     если стало намбер, то числа, которые добавляються в num2
+
+//     - вводим первое число (строка),
+//     - когда нажимаем оператор:
+//     1. первое число из строки переводим в намбер
+//     2. хоть 100 раз нажимаем оператор - добавляется только последний
+//     3. проверяем если есть результат, тогда обнуляем второе число и результат,
+//     если нет, то просто работаем
+
+//     - вводим второе число (строка)
+//    - когда нажимаем = второе число переводим в намбер 
+//    - нажимаем =
+//    1. проводим операцию:
+//     2. результат добавляем в result, который был false
+//     3. num1 равняем результату
+//     4. если нажимаем = опять, то повторяется операция
+//     в итоге 2 + 2 = 4;
+//     4 + 2 = 6;
+
+
+// 2+2-
+// 2 + 2 = 4;
+
+    
+    // else if (secondNumber !== '') {
+    //     operate();
+    // }
     // console.log(secondNumber);
     // if(trigger >= 1 && secondNumber !== ''){
     //     operate();
@@ -94,8 +131,14 @@ function operate(){
             }
         break;
     }
+    // console.log({firstNumber});
+    // console.log({secondNumber});
+    // console.log({result});
+    // currentScreen.innerText = result;
+
     resultScreen.innerText = result;
     firstNumber = result;
+  
 }
 equals.addEventListener('click', operate);
 
